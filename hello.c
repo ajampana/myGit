3ca0a41510a551bfd136e9fc7ce2a06641f2f689
Deepiga C Ravi <dcravi@my.okcu.edu>
2015-03-12 19:17:50 -0500

    modified by Deepiga

--- hello.c
+++ hello.c
@@ -6,5 +6,7 @@ printf("This is the Second Modification to the hello code to check git\n");
 =======
 	printf("This is my first use of git \n");
 	printf("This is a second modification \n");
+	printf("this is my third modification\n");
+	pritnf("Pulled the team member file\n");
 >>>>>>> 449a99784435359472ca53ff022c586ad84fab1b
 }
